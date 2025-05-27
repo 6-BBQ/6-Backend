db222211adeaefcb1ed086138847e2e664ff9b39
smg0218 <smg000218@gmail.com>
2025-05-27 15:45:06 +0900

    chore : 여러 계정에 들어가있을 경우 중복되서 캐릭터가 출력되는 현상 수정(2차)

--- src/main/java/com/sixbbq/gamept/characterRegist/repository/CharacterRegistRepository.java
+++ src/main/java/com/sixbbq/gamept/characterRegist/repository/CharacterRegistRepository.java
@@ -3,6 +3,7 @@ package com.sixbbq.gamept.characterRegist.repository;
 import com.sixbbq.gamept.characterRegist.entity.CharacterRegist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,8 +12,17 @@ import java.util.Optional;
 @Repository
 public interface CharacterRegistRepository extends JpaRepository<CharacterRegist, Long> {
     List<CharacterRegist> findByUserId(String userId);
-    @Query("SELECT DISTINCT c.characterId FROM CharacterRegist c WHERE c.adventureName = :adventureName")
-    List<CharacterRegist> findDistinctCharacterIdByAdventureName(String adventureName);
+    @Query(value = """
+    SELECT * FROM characters c
+    WHERE c.adventure_name = :adventureName
+      AND c.id IN (
+          SELECT MIN(id) FROM characters
+          WHERE adventure_name = :adventureName
+          GROUP BY character_id
+      )
+""", nativeQuery = true)
+    List<CharacterRegist> findDistinctCharacterIdByAdventureName(@Param("adventureName") String adventureName);
+
     boolean existsByUserIdAndCharacterId(String userId, String characterId);
     Optional<CharacterRegist> findByUserIdAndCharacterId(String userId, String characterId);
 }
