40207d7158cbbf1249f350d2c1cf7a6fa38daf12
smg0218 <smg000218@gmail.com>
2025-05-29 15:26:13 +0900

    feat : 서버 종료 관련 알림기능 추가(JVM에 의해 다운되는 경우)

--- src/main/java/com/sixbbq/gamept/exception/controller/GlobalExceptionController.java
+++ src/main/java/com/sixbbq/gamept/exception/controller/GlobalExceptionController.java
@@ -1,8 +1,8 @@
 package com.sixbbq.gamept.exception.controller;
 
+import com.sixbbq.gamept.util.ErrorUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
@@ -23,6 +23,12 @@ public class GlobalExceptionController {
 
     @ExceptionHandler(RestClientException.class)
     public ResponseEntity<?> handleRestClientException(RestClientException e, HttpServletRequest request) {
+        return ResponseEntity.internalServerError().body(e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntimeException(RuntimeException e, HttpServletRequest request) {
+        ErrorUtil.logError(e, request, "에러발생");
         return ResponseEntity.internalServerError().body(e.getMessage());
     }
 
